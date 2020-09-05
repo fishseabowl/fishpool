6036639a2ae4e6043b797ec0aff209294788050d
xuefei han <xuefei@hp-pavilion-sleekbook-14-pc.hsd1.ca.comcast.net>
2020-09-04 19:29:42 -0700

    Update the required functions

--- kamendia/binary.go
+++ kamendia/binary.go
@@ -6,7 +6,7 @@ import (
 	"sort"
 )
 
-// XOR return the result of XOR(a, b).
+// XOR allocates a new byte slice with the result of XOR(a, b).
 func XOR(a, b []byte) []byte {
 	if len(a) != len(b) {
 		return a
@@ -21,27 +21,7 @@ func XOR(a, b []byte) []byte {
 	return c
 }
 
-// PrefixDiff counts the number of equal prefixed bits of a and b.
-func PrefixDiff(a, b []byte, n int) int {
-	buf, total := XOR(a, b), 0
-
-	for i, b := range buf {
-		if 8*i >= n {
-			break
-		}
-
-		if n > 8*i && n < 8*(i+1) {
-			shift := 8 - uint(n%8)
-			b >>= shift
-		}
-
-		total += bits.OnesCount8(b)
-	}
-
-	return total
-}
-
-// PrefixLen returns the number of prefixed zero bits of a.
+// PrefixLen returns the number of prefixed zero bits in a.
 func PrefixLen(a []byte) int {
 	for i, b := range a {
 		if b != 0 {
@@ -53,7 +33,7 @@ func PrefixLen(a []byte) int {
 }
 
 // SortByDistance sorts ids by descending XOR distance with respect to id.
-func SortByDistance(id PublicKey, ids []ID) []ID {
+func SortByDistance(id ID, ids []ID) []ID {
 	sort.Slice(ids, func(i, j int) bool {
 		return bytes.Compare(XOR(ids[i].ID[:], id[:]), XOR(ids[j].ID[:], id[:])) == -1
 	})
